034095d883f4b2d748dee2fb6f97ffe05bfb98f1
松崎仁美 <h.matsuzaki@peacs.co.jp>
2021-06-16 22:36:36 +0900

    TOPボタンスクロール指示削除

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,6 @@ $(function(){
 
   // トップへ戻るボタン //
   var pagetop = $('.top-btn');
-  pagetop.hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 400) {
-      pagetop.fadeIn();
-    } else {
-      pagetop.fadeOut();
-    }
-  });
   pagetop.click(function () {
     $('body, html').animate({ scrollTop: 0 }, 500);
     return false;

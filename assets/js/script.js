855d5a0ddf7e782ccd7c00c52540839b0e6b9751
松崎仁美 <h.matsuzaki@peacs.co.jp>
2021-06-28 19:32:10 +0900

    margin設定

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 $(function(){
 
+  // hoverイベント //
+  $('.logo a, .artist, .n-disc a, .acco-head, .top-btn')
+  .on('touchstart', function(){
+    $(this).addClass('hover');
+  }).on('touchend', function(){
+    $(this).removeClass('hover');
+  });
+
+
+  // ハンバーガーメニュー //
   $('.hamburger').click(function() {
     $('.hamburger').toggleClass("active");
     $('nav').slideToggle();
@@ -19,14 +29,14 @@ $(function(){
 
 
   //.accordion_oneの中の.accordion_headerがクリックされたら
-   $('.biography .accordion .acco-head').click(function(){
-     //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
-     $(this).next('.acco-inner').slideToggle();
-     $(this).toggleClass("open");
-     //クリックされた.accordion_oneの中の.accordion_header以外の.accordion_oneの中の.accordion_headerに隣接する.accordion_oneの中の.accordion_innerを閉じる
-     $('.biography .accordion .acco-head').not($(this)).next('.accordion .acco-inner').slideUp();
-     $('.biography .accordion .acco-head').not($(this)).removeClass("open");
-   });
+  $('.biography .accordion .acco-head').click(function(){
+    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+    $(this).next('.acco-inner').slideToggle();
+    $(this).toggleClass("open");
+    //クリックされた.accordion_oneの中の.accordion_header以外の.accordion_oneの中の.accordion_headerに隣接する.accordion_oneの中の.accordion_innerを閉じる
+    $('.biography .accordion .acco-head').not($(this)).next('.accordion .acco-inner').slideUp();
+    $('.biography .accordion .acco-head').not($(this)).removeClass("open");
+  });
 
 
 });
